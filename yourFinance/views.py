c3b86d945b82203c7b66f627591cbe18aaae9102
InPursuitPL <inpursuitpl@gmail.com>
2017-03-31 11:00:04 +0200

    Added initialization of stashes names

--- yourFinance/views.py
+++ yourFinance/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.forms import formset_factory
+from django.forms import modelformset_factory
 
 from .models import Stash
 from .forms import RegistrationForm, StashForm
+
+def make_choices_list(elementName, choicesTuple):
+	list = []
+	for i in range(len(choicesTuple)):
+		list.append({elementName:  choicesTuple[i][0]})
+	return list
+
 
 def index(request):
     return render(request, 'yourFinance/index.html')
@@ -26,7 +33,7 @@ def register_page(request):
 
 @login_required
 def add_data(request):
-    StashFormSet = formset_factory(StashForm, extra=len(Stash.NAME_CHOICES))
+    StashFormSet = modelformset_factory(Stash, form=StashForm, extra=4)
     if request.method == 'POST':
         formset = StashFormSet(request.POST)
         if formset.is_valid():
@@ -37,11 +44,8 @@ def add_data(request):
 
         else:
             return render(request, 'yourFinance/add_data.html', {'formset': formset})
-    formset = StashFormSet(initial=[
-        {'name': Stash.NAME_CHOICES[0][0],
-         'date': '2009-11-11',
-         'amount': 456}
-    ])
+    formset = StashFormSet(queryset=Stash.objects.none(),
+                           initial=make_choices_list('name', Stash.NAME_CHOICES))
     return render(request, 'yourFinance/add_data.html', {'formset': formset})
 
 @login_required
